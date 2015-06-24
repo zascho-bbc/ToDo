fc5a037d6b760abc6702116dba6f648bcc30471b
Miguel Angelo Gomes <Miguel.Jorge@bbcag.ch>
2015-06-24 15:33:32 +0200

    cooonflicts
    coonflicts

---
+++ app/src/main/java/ch/bbcag/todo/AlertReceiver.java
@@ -0,0 +1,41 @@
+package ch.bbcag.todo;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+/**
+ * Created by zjorgm on 19.06.2015.
+ */
+public class AlertReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context k1, Intent k2) {
+        // TODO Auto-generated method stub
+        createNotification(k1, "Times Up", "5 seconds has passed", "Alert");
+    }
+
+    public void createNotification(Context context, String msg, String msgtext, String msgalert){
+        PendingIntent notificIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0);
+
+        NotificationCompat.Builder mbuilder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.java)
+                .setContentTitle(msg)
+                .setTicker(msgalert)
+                .setContentText(msgtext);
+
+        mbuilder.setContentIntent(notificIntent);
+        mbuilder.setDefaults(NotificationCompat.DEFAULT_SOUND);
+        mbuilder.setAutoCancel(true);
+
+        NotificationManager mnotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mnotificationManager.notify(1,mbuilder.build());
+
+    }
+}
