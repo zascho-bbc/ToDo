5ce0adad32c9f83b14a04b69eadf0c442ef7094f
zjorgm <Miguel.Jorge@bbcag.ch>
2015-07-08 15:40:33 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/src/main/java/ch/bbcag/todo/database/AufgabenDAO.java

--- app/src/main/java/ch/bbcag/todo/Fragments/AufgabeAnsicht_Fragment.java
+++ app/src/main/java/ch/bbcag/todo/Fragments/AufgabeAnsicht_Fragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
